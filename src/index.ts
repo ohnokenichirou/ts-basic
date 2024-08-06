665d013c2d1533a130317bf773a237a839306d34
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-08-06 17:03:13 +0900

    basicフォルダ追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import World from './world'
+// import World from './world'
 
-const root: HTMLElement | null = document.getElementById('root')
-const world = new World('Hello World!!!')
-world.sayHello(root)
+// const root: HTMLElement | null = document.getElementById('root')
+// const world = new World('Hello World!!!')
+// world.sayHello(root)
+
+// 03.基本の型定義
+import { primitiveSample } from './basic'
